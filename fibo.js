d55ddf60011acb74b013e3586b9f44230ee599e6
Sergei Chernik <sergey.chernik@maximumtest.ru>
2020-07-10 00:20:02 +0300

    implement recursive algoritm

--- fibo.js
+++ fibo.js
@@ -1,21 +1,17 @@
 const fibNum = (n) => {
-	let a = 1;
-	let b = 1;
-	for (let i = 3; i <= n; i++) {
-    let c = a + b;
-    a = b;
-    b = c;
+  if (n <= 2) {
+    return 1;
   }
-  return b;
+return fibNum(n-1) + fibNum(n-2);
 }
 
-const fibArray = (n) => {
+const fibSequence = (n) => {
 	let result = [];
   let i = 1;
   while (i <= n) {
     let currentFib = fibNum(i);
-    i++;
     result.push(currentFib);
+    i++;
   }
   return result;
 }
